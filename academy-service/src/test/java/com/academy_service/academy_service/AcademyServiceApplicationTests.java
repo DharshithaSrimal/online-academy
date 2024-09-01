afa67f5c93fe4970845d9d76f0f762c57191eb50
Dharshitha Senevirathne <dharshithasrimal@gmail.com>
2024-09-01 23:44:26 +0530

    Academy service

---
+++ academy-service/src/test/java/com/academy_service/academy_service/AcademyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academy_service.academy_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ academy-user-service/src/main/java/com/academy/user/academy_user_service/AcademyUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.academy.user.academy_user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademyUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademyUserServiceApplication.class, args);
+	}
+
+}
