03b46b8c2a5282a5dd8d0fead82077d17408d44b
Dharshitha Senevirathne <dharshithasrimal@gmail.com>
2024-10-15 16:42:29 +0530

    Cloud Gateway
    Initial commit

---
+++ academy-service/src/main/java/com/online_academy/academy_service/dto/UserDTO.java
@@ -0,0 +1,15 @@
+package com.online_academy.academy_service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+    private Long id;
+    private String email;
+    private String name;
+    private String role;
+}
